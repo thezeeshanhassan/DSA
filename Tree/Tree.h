9ab67a33a8e13b4028c8bf36161edae767a05bc5
= <=>
2024-03-13 17:28:41 +0500

    Tree Upto Min Height Done

---
+++ Tree/Tree.h
@@ -0,0 +1,23 @@
+#ifndef Tree_h
+#define Tree_h
+#include "TreeNode.h"
+
+class Tree
+{
+private:
+    TreeNode *root;
+
+public:
+    Tree() : root(nullptr){};
+    TreeNode *createTreeR();
+    void create();
+    void preOrder();
+    void preOrderTraversal(TreeNode *);
+    void levelOrderTraversal(TreeNode *);
+    void levelOrder();
+    int maxDepthofBinaryTree(TreeNode *);
+    void maxDepth();
+    int minepthofBinaryTree(TreeNode *&);
+    void minDepth();
+};
+#endif
